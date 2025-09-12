2ff401914742c9f516da6dd76823385bfbdc3b16
Vyacheslav Butyrkin <slava.butyrkin@gmail.com>
2025-09-12 11:51:27 +0200

    k8s added

---
+++ .terraform.lock.hcl
@@ -0,0 +1,43 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/kubernetes" {
+  version     = "2.38.0"
+  constraints = "~> 2.38"
+  hashes = [
+    "h1:1OF0rDUteVdoX04BrtmTc0T4NOo6+D0utmK6hM0EzZw=",
+    "zh:0af928d776eb269b192dc0ea0f8a3f0f5ec117224cd644bdacdc682300f84ba0",
+    "zh:1be998e67206f7cfc4ffe77c01a09ac91ce725de0abaec9030b22c0a832af44f",
+    "zh:326803fe5946023687d603f6f1bab24de7af3d426b01d20e51d4e6fbe4e7ec1b",
+    "zh:4a99ec8d91193af961de1abb1f824be73df07489301d62e6141a656b3ebfff12",
+    "zh:5136e51765d6a0b9e4dbcc3b38821e9736bd2136cf15e9aac11668f22db117d2",
+    "zh:63fab47349852d7802fb032e4f2b6a101ee1ce34b62557a9ad0f0f0f5b6ecfdc",
+    "zh:924fb0257e2d03e03e2bfe9c7b99aa73c195b1f19412ca09960001bee3c50d15",
+    "zh:b63a0be5e233f8f6727c56bed3b61eb9456ca7a8bb29539fba0837f1badf1396",
+    "zh:d39861aa21077f1bc899bc53e7233262e530ba8a3a2d737449b100daeb303e4d",
+    "zh:de0805e10ebe4c83ce3b728a67f6b0f9d18be32b25146aa89116634df5145ad4",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+    "zh:faf23e45f0090eef8ba28a8aac7ec5d4fdf11a36c40a8d286304567d71c1e7db",
+  ]
+}
+
+provider "registry.terraform.io/yandex-cloud/yandex" {
+  version     = "0.158.0"
+  constraints = ">= 0.158.0"
+  hashes = [
+    "h1:Wmlc8h+50x/8Xz60W65Mh5uylJy7aGfEwxUhwrH+1uw=",
+    "zh:03a51a39772415140bd7e0a15455ad04814776701c53c51009caef8df9278433",
+    "zh:052965b55fef1add1f66b651dd110024ec45b143491bede2356f755d5a58f80d",
+    "zh:0e29db3592ea0517cf67ca31f6cd9eb84771dd8bb307809e3ac650097eb76fc6",
+    "zh:27798c9c9e00deb762ba5a034de4f8aefec9e94d1fc00e88420a909826dc7da5",
+    "zh:638491957db491fabed18ba1eec373afca55b73ef2ad3dbff5a2fdb01b7dc09a",
+    "zh:6af99ee64f8f7a4a917b5ad9988515eee64c339649548a6a4b593d5c12370956",
+    "zh:7d9c1825b705f37368d157e734f6ec8a05dbee3bbcf1feac6a395ff7c47bedcc",
+    "zh:bc1173074cb63c364564a346e2903f5c7aae08b2f9fd2318b8a511f86e388520",
+    "zh:c24decc98ae7c29ef2e1e96b5e5e15c41a0a8cc3ed3d940350d473a6c32bd730",
+    "zh:c7ce8e60a15c3a5d6657dfc11439d6b3e48244ca664dd81e09777810b5700812",
+    "zh:d2005a80fc03fda5ac6e52527116587c22e1f79dd85951ddcb57392b88d72a2b",
+    "zh:e3c6c84b5a9b0073d18d4bd8f05c11426ec18eb827ec376157777d98c90efcdc",
+    "zh:eb9746cf0446397c15052493adb2e46a96b07429b27ae5041615a1105dab3e28",
+  ]
+}
--- hw06-4.tf
+++ hw06-4.tf
@@ -6,12 +6,178 @@
 # vpc.user
 # vpc.securityGroups.admin - для создания security group
 # compute.admin - для создания группы ВМ
+# k8s.admin - для создания кластера k8s
 
 
 # ---------------------- Поиск существующего реестра YCR --------------------
-# Находим реестр по имени в нужной папке
 data "yandex_container_registry" "cr" {
   name      = "hw06-cr-demo"
   folder_id = var.folder_id
 }
 
+# ---------------------- Сеть и подсети --------------------
+resource "yandex_vpc_network" "hw06_mk8s_vpc" {
+  name = "hw06-mk8s-vpc"
+}
+
+resource "yandex_vpc_subnet" "hw06_mk8s_subnet_a" {
+  name           = "hw06-mk8s-subnet-a"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.hw06_mk8s_vpc.id
+  v4_cidr_blocks = ["10.90.1.0/24"]
+}
+
+resource "yandex_vpc_subnet" "hw06_mk8s_subnet_b" {
+  name           = "hw06-mk8s-subnet-b"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.hw06_mk8s_vpc.id
+  v4_cidr_blocks = ["10.90.2.0/24"]
+}
+
+# ---------------------- Сервисные аккаунты --------------------
+resource "yandex_iam_service_account" "hw06_mk8s_sa_master" {
+  name = "hw06-mk8s-master-sa"
+}
+
+resource "yandex_iam_service_account" "hw06_mk8s_sa_nodes" {
+  name = "hw06-mk8s-nodes-sa"
+}
+
+# ---------------------- Роли для мастер-SA --------------------
+resource "yandex_resourcemanager_folder_iam_member" "hw06_mk8s_sa_master_k8s_agent" {
+  folder_id = var.folder_id
+  role      = "k8s.clusters.agent"
+  member    = "serviceAccount:${yandex_iam_service_account.hw06_mk8s_sa_master.id}"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "hw06_mk8s_sa_master_vpc_public_admin" {
+  folder_id = var.folder_id
+  role      = "vpc.publicAdmin"
+  member    = "serviceAccount:${yandex_iam_service_account.hw06_mk8s_sa_master.id}"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "hw06_mk8s_sa_master_lb_admin" {
+  folder_id = var.folder_id
+  role      = "load-balancer.admin"
+  member    = "serviceAccount:${yandex_iam_service_account.hw06_mk8s_sa_master.id}"
+}
+
+# ---------------------- Роли для node-SA --------------------
+resource "yandex_container_registry_iam_binding" "hw06_mk8s_nodes_puller_on_registry" {
+  registry_id = data.yandex_container_registry.cr.id
+  role        = "container-registry.images.puller"
+  members     = ["serviceAccount:${yandex_iam_service_account.hw06_mk8s_sa_nodes.id}"]
+}
+
+# ---------------------- Кластер Kubernetes --------------------
+resource "yandex_kubernetes_cluster" "hw06_mk8s_cluster" {
+  name       = "hw06-mk8s-cluster"
+  folder_id  = var.folder_id
+  network_id = yandex_vpc_network.hw06_mk8s_vpc.id
+
+  master {
+    version = "1.32"
+
+    zonal {
+      zone      = var.zone
+      subnet_id = yandex_vpc_subnet.hw06_mk8s_subnet_a.id
+    }
+
+    public_ip = true
+  }
+
+  service_account_id      = yandex_iam_service_account.hw06_mk8s_sa_master.id
+  node_service_account_id = yandex_iam_service_account.hw06_mk8s_sa_nodes.id
+
+  release_channel         = "REGULAR"
+  network_policy_provider = "CALICO"
+
+  cluster_ipv4_range = "10.96.0.0/16"
+  service_ipv4_range = "10.97.0.0/16"
+
+  labels = {
+    env   = "demo"
+    owner = "terraform"
+  }
+
+  depends_on = [
+    yandex_resourcemanager_folder_iam_member.hw06_mk8s_sa_master_k8s_agent,
+    yandex_resourcemanager_folder_iam_member.hw06_mk8s_sa_master_vpc_public_admin,
+    yandex_resourcemanager_folder_iam_member.hw06_mk8s_sa_master_lb_admin,
+    yandex_container_registry_iam_binding.hw06_mk8s_nodes_puller_on_registry,
+  ]
+}
+
+# ---------------------- Группа узлов --------------------
+resource "yandex_kubernetes_node_group" "hw06_mk8s_nodes" {
+  name       = "hw06-mk8s-nodes"
+  cluster_id = yandex_kubernetes_cluster.hw06_mk8s_cluster.id
+
+  scale_policy {
+    fixed_scale {
+      size = 2
+    }
+  }
+
+  allocation_policy {
+    location { zone = var.zone }
+  }
+
+  instance_template {
+    platform_id = "standard-v2"
+
+    resources {
+      cores         = 2
+      memory        = 2
+      core_fraction = 20
+    }
+
+    boot_disk {
+      type = "network-hdd"
+      size = 20
+    }
+
+    network_interface {
+      subnet_ids = [yandex_vpc_subnet.hw06_mk8s_subnet_a.id]
+      nat        = true
+    }
+
+    scheduling_policy {
+      preemptible = true
+    }
+
+    metadata = {
+      "ssh-keys" = "ubuntu:ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEIwAhe9IhThZ8Ed/bZ6h/3CPfX4hhh3DppnRFCadA6L slava.butyrkin@gmail.com"
+    }
+  }
+
+  maintenance_policy {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  labels = {
+    role  = "app"
+    infra = "yc"
+  }
+
+  node_taints = []
+}
+
+# ---------------------- Outputs --------------------
+output "cluster_id" {
+  value = yandex_kubernetes_cluster.hw06_mk8s_cluster.id
+}
+
+output "cluster_name" {
+  value = yandex_kubernetes_cluster.hw06_mk8s_cluster.name
+}
+
+output "node_group_id" {
+  value = yandex_kubernetes_node_group.hw06_mk8s_nodes.id
+}
+
+output "kubeconfig_hint" {
+  description = "Команда для получения kubeconfig"
+  value       = "yc managed-kubernetes cluster get-credentials ${yandex_kubernetes_cluster.hw06_mk8s_cluster.name} --external --force"
+}
